b055d97b3d33b3d0913f072289589d45e86dc6f8
Daniel Carr <d.douglas.carr@gmail.com>
2016-05-15 10:32:40 +1000

    working

--- countdown.sh
+++ countdown.sh
@@ -3,27 +3,29 @@
 # ./countdown <num of unit> <unit> <msg>
 # eg:
 # ./countdown 25 minutes Have a Break!
-seconds=0
-unit=$1
-value=$2
+timer_length=0
+value=$1
+unit=$2
+msg=$3
 
 case $unit in 
 
-  "s" | "seconds" | "seconds" )   seconds=$value
+  "s" | "seconds" | "seconds" )   timer_length=$value
                                   ;;
-  "m" | "minute" | "minutes" )    seconds=$(($value * 60))
+  "m" | "minute" | "minutes" )    timer_length=$(($value * 60))
                                   ;;
-  "h" | "hour" | "hours" )        seconds=$(($value * 60 * 60))
+  "h" | "hour" | "hours" )        timer_length=$(($value * 60 * 60))
                                   ;;
   * )                             echo "unknown unit: $unit"
                                   exit 1;
                                   ;;
 esac
 
-finished_date=$((`date +%s` + $seconds))
+finished_date=$((`date +%s` + $timer_length))
 
 while [ "$finished_date" -ne `date +%s` ]; do
-  echo -ne "$(date --date @$(($seconds - `date +%s` - 19800 )) +%H:%M:%S)\r";
+  timer_remaining=$(($finished_date - `date +%s`))
+  echo -ne "$(date --utc --date @$timer_remaining +%H:%M:%S)\r"
 done
 
-notify-send 'Timer finished' "$*"
+notify-send 'Timer finished' "$msg"
